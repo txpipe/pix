8580d0ab91345b0bf8c99a4929cd6b7dd4c0de94
rvcas <x@rvcas.dev>
2022-01-19 14:04:13 -0500

    feat: create a merge helper to hide away x, y

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 use std::io;
 
-use image::imageops;
+use image::{imageops, GenericImage, GenericImageView, RgbaImage};
 
 use nft_gen::cli::Args;
 
 fn main() -> Result<(), io::Error> {
     let args = Args::new();
 
-    let mut image1 = image::open("example/image1.png").unwrap();
+    let image1 = image::open("example/image1.png").unwrap();
+
+    let (width, height) = image1.dimensions();
+
+    let mut base = RgbaImage::new(width, height);
+
     let image2 = image::open("example/image2.png").unwrap();
 
-    imageops::overlay(&mut image1, &image2, 0, 0);
+    merge(&mut base, &image1);
+    merge(&mut base, &image2);
 
-    image1.save("example/output.png").unwrap();
+    base.save("example/output.png").unwrap();
 
-    println!("{:#?}", args);
+    for n in 1..=args.amount {
+        println!("{}", n);
+    }
 
     Ok(())
+}
+
+fn merge<I, J>(bottom: &mut I, top: &J)
+where
+    I: GenericImage,
+    J: GenericImageView<Pixel = I::Pixel>,
+{
+    imageops::overlay(bottom, top, 0, 0);
 }

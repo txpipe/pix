ddc9b8e7997a8d8f91b72e75dcf1b002d3c7313c
rvcas <x@rvcas.dev>
2022-01-19 22:51:29 -0500

    Updates to CLI Interface
    * amount is now an option
      * now defaults to 10000
    * added path
      defaults to images/

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,14 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct Args {
+    #[clap(short, long, default_value = "10000")]
     pub amount: usize,
+
+    #[clap(short, long, default_value = "images")]
+    pub path: PathBuf,
 }
 
 impl Args {
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 use std::io;
 
-use image::{imageops, GenericImage, GenericImageView, RgbaImage};
+use image::{imageops, GenericImage, GenericImageView};
 
 use nft_gen::cli::Args;
 
 fn main() -> Result<(), io::Error> {
     let args = Args::new();
 
-    let image1 = image::open("example/image1.png").unwrap();
+    let image1_path = args.path.join("image1.png");
 
-    let (width, height) = image1.dimensions();
+    let mut image1 = image::open(image1_path).unwrap();
 
-    let mut base = RgbaImage::new(width, height);
+    let image2_path = args.path.join("image2.png");
 
-    let image2 = image::open("example/image2.png").unwrap();
+    let image2 = image::open(image2_path).unwrap();
 
-    merge(&mut base, &image1);
-    merge(&mut base, &image2);
+    merge(&mut image1, &image2);
 
-    base.save("example/output.png").unwrap();
-
-    for n in 1..=args.amount {
-        println!("{}", n);
-    }
+    image1.save("example/output.png").unwrap();
 
     Ok(())
 }

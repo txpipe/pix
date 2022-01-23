92417992f1055cbab9a523ad4e7652416704a9a6
rvcas <x@rvcas.dev>
2022-01-23 01:50:42 -0500

    feat: some basic looping for simple mode

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,78 @@
 use image::{imageops, GenericImage, GenericImageView, RgbaImage};
 
-use nft_gen::{cli::Args, traits::Traits};
+use nft_gen::{
+    cli::Args,
+    traits::{SimpleTrait, Traits},
+};
 
 fn main() -> Result<(), String> {
     let args = Args::new();
 
-    // let traits = Traits::new();
+    let (mut x, mut y) = (0, 0);
 
-    let mut images = Vec::new();
+    let order = vec![
+        "background",
+        "eyes",
+        "Base",
+        "Stitch Color",
+        "belly",
+        "forehead",
+        "Stuffing",
+    ];
+
+    let mut traits = Traits::new();
 
     for entry in args.path.read_dir().expect("path is not a directory") {
         if let Ok(entry) = entry {
-            let image = image::open(entry.path()).expect("file not found");
+            let mut simple_trait = SimpleTrait::new();
 
-            images.push(image);
+            let dir_path = entry.path();
+
+            for rarity in dir_path
+                .read_dir()
+                .expect("trait must be a folder")
+                .filter(|x| x.as_ref().unwrap().path().is_dir())
+            {
+                if let Ok(rarity) = rarity {
+                    let rarity_path = rarity.path();
+
+                    for trait_file in rarity_path.read_dir().expect("rarity must be a directory") {
+                        if let Ok(trait_file) = trait_file {
+                            let trait_path = trait_file.path();
+
+                            let image = image::open(trait_path).expect("file not found");
+
+                            let (width, height) = image.dimensions();
+
+                            x = width;
+                            y = height;
+
+                            match rarity_path.file_name().unwrap().to_str() {
+                                Some("common") => simple_trait.common.push(image),
+                                Some("uncommon") => simple_trait.uncommon.push(image),
+                                Some("rare") => simple_trait.rare.push(image),
+                                Some("mythical") => simple_trait.mythical.push(image),
+                                Some("legendary") => simple_trait.legendary.push(image),
+                                _ => (),
+                            }
+                        }
+                    }
+                }
+            }
+
+            traits.insert(dir_path, simple_trait);
         }
     }
 
-    let (x, y) = images[0].dimensions();
-
     let mut base = RgbaImage::new(x, y);
 
-    for image in images {
-        merge(&mut base, &image);
+    for item in order {
+        let simple_trait = traits.get(&args.path.join(item)).unwrap();
+
+        merge(&mut base, &simple_trait.legendary[0]);
     }
 
-    base.save("example/output.png").unwrap();
+    base.save("output2.png").unwrap();
 
     Ok(())
 }
--- src/traits.rs
+++ src/traits.rs
@@ -1,11 +1,26 @@
+use std::{collections::HashMap, path::PathBuf};
+
 use image::DynamicImage;
 
-pub type Traits = Vec<SimpleTrait>;
+pub type Traits = HashMap<PathBuf, SimpleTrait>;
 
+#[derive(Debug)]
 pub struct SimpleTrait {
     pub common: Vec<DynamicImage>,
     pub uncommon: Vec<DynamicImage>,
     pub rare: Vec<DynamicImage>,
     pub mythical: Vec<DynamicImage>,
     pub legendary: Vec<DynamicImage>,
+}
+
+impl SimpleTrait {
+    pub fn new() -> Self {
+        SimpleTrait {
+            common: Vec::new(),
+            uncommon: Vec::new(),
+            rare: Vec::new(),
+            mythical: Vec::new(),
+            legendary: Vec::new(),
+        }
+    }
 }

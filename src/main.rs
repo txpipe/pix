16835addd57ea56daa124291899e9093f220b0f7
rvcas <x@rvcas.dev>
2022-01-24 00:36:00 -0500

    feat: refactor trait loading into a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use image::{imageops, GenericImage, GenericImageView, RgbaImage};
 
 use nft_gen::{
-    cli::Commands,
+    cli::{Commands, Mode},
     config::Config,
     traits::{Trait, Traits},
 };
@@ -17,80 +17,11 @@ fn main() -> Result<(), String> {
             todo!("implement new command")
         }
         Commands::Gen(args) => {
-            let (mut x, mut y) = (0, 0);
-
             let config = Config::new(&args.config);
 
             let mut traits = Traits::new();
 
-            for entry in config
-                .path
-                .read_dir()
-                .expect("path is not a directory")
-                .filter(|x| x.as_ref().unwrap().path().is_dir())
-            {
-                if let Ok(entry) = entry {
-                    let mut trait_list = Vec::new();
-
-                    let dir_path = entry.path();
-
-                    for rarity in dir_path
-                        .read_dir()
-                        .expect("trait must be a folder")
-                        .filter(|x| x.as_ref().unwrap().path().is_dir())
-                        .filter(|x| {
-                            RARITIES
-                                .iter()
-                                .any(|y| x.as_ref().unwrap().path().ends_with(y))
-                        })
-                    {
-                        if let Ok(rarity) = rarity {
-                            let rarity_path = rarity.path();
-
-                            for trait_file in rarity_path
-                                .read_dir()
-                                .expect("rarity must be a directory")
-                                .filter(|x| x.as_ref().unwrap().path().is_file())
-                                .filter(|x| {
-                                    x.as_ref()
-                                        .unwrap()
-                                        .path()
-                                        .extension()
-                                        .unwrap()
-                                        .to_str()
-                                        .unwrap()
-                                        == "png"
-                                })
-                            {
-                                if let Ok(trait_file) = trait_file {
-                                    let trait_path = trait_file.path();
-
-                                    let image = image::open(trait_path).expect("file not found");
-
-                                    let (width, height) = image.dimensions();
-
-                                    x = width;
-                                    y = height;
-
-                                    trait_list.push(Trait {
-                                        image,
-                                        weight: match rarity_path.file_name().unwrap().to_str() {
-                                            Some("common") => 70,
-                                            Some("uncommon") => 50,
-                                            Some("rare") => 20,
-                                            Some("mythical") => 10,
-                                            Some("legendary") => 5,
-                                            _ => unreachable!(),
-                                        },
-                                    })
-                                }
-                            }
-                        }
-                    }
-
-                    traits.insert(dir_path, trait_list);
-                }
-            }
+            let (x, y) = load_traits(&mut traits, &config);
 
             let mut base = RgbaImage::new(x, y);
 
@@ -117,4 +48,86 @@ where
     J: GenericImageView<Pixel = I::Pixel>,
 {
     imageops::overlay(bottom, top, 0, 0);
+}
+
+fn load_traits(traits: &mut Traits, config: &Config) -> (u32, u32) {
+    let (mut x, mut y) = (0, 0);
+
+    let feature_paths = config
+        .path
+        .read_dir()
+        .expect("path is not a directory")
+        .filter(|x| x.as_ref().unwrap().path().is_dir());
+
+    for feature_dir in feature_paths {
+        let category_dir = feature_dir.unwrap();
+        let category_path = category_dir.path();
+
+        let mut trait_list = Vec::new();
+
+        match config.mode {
+            Mode::Advanced => todo!("implement advanced mode"),
+            Mode::Simple => {
+                let rarity_paths = category_path
+                    .read_dir()
+                    .expect("trait must be a folder")
+                    .filter(|x| x.as_ref().unwrap().path().is_dir())
+                    .filter(|x| {
+                        RARITIES
+                            .iter()
+                            .any(|y| x.as_ref().unwrap().path().ends_with(y))
+                    });
+
+                for rarity_dir in rarity_paths {
+                    let rarity_dir = rarity_dir.unwrap();
+                    let rarity_path = rarity_dir.path();
+
+                    let trait_paths = rarity_path
+                        .read_dir()
+                        .expect("rarity must be a directory")
+                        .filter(|x| x.as_ref().unwrap().path().is_file())
+                        .filter(|x| {
+                            x.as_ref()
+                                .unwrap()
+                                .path()
+                                .extension()
+                                .unwrap()
+                                .to_str()
+                                .unwrap()
+                                == "png"
+                        });
+
+                    for trait_file in trait_paths {
+                        let trait_file = trait_file.unwrap();
+                        let trait_path = trait_file.path();
+
+                        let image = image::open(trait_path).expect("file not found");
+
+                        let (width, height) = image.dimensions();
+
+                        if x == 0 && y == 0 {
+                            x = width;
+                            y = height;
+                        }
+
+                        trait_list.push(Trait {
+                            image,
+                            weight: match rarity_path.file_name().unwrap().to_str() {
+                                Some("common") => 70,
+                                Some("uncommon") => 50,
+                                Some("rare") => 20,
+                                Some("mythical") => 10,
+                                Some("legendary") => 5,
+                                _ => unreachable!(),
+                            },
+                        })
+                    }
+                }
+            }
+        }
+
+        traits.insert(category_path, trait_list);
+    }
+
+    (x, y)
 }
